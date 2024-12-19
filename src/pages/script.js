2d9d72f68a2823b8288a3875377c9fac7cd41f10
Andrea <anto0002@stud.kea.dk>
2024-12-19 10:41:09 +0100

    js debug

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,83 +1,83 @@
-// hent dropdown knap //
-const button = document.querySelector(".dropdown #knap1");
-const dropDownContent = document.querySelector(".content");
+// // hent dropdown knap //
+// const button = document.querySelector(".dropdown #knap1");
+// const dropDownContent = document.querySelector(".content");
 
-// tilføj klik-event //
-button.addEventListener("click", () => {
-  // tjek om dropdown allerede er vist //
-  if (dropDownContent.style.display === "block") {
-    dropDownContent.style.display = "none";
-  } else {
-    dropDownContent.style.display = "block";
-  }
-});
+// // tilføj klik-event //
+// button.addEventListener("click", () => {
+//   // tjek om dropdown allerede er vist //
+//   if (dropDownContent.style.display === "block") {
+//     dropDownContent.style.display = "none";
+//   } else {
+//     dropDownContent.style.display = "block";
+//   }
+// });
 
-// luk dropdown hvis der klikkes udenfor //
-window.addEventListener("click", (event) => {
-  if (!event.target.matches(".dropdown #knap1")) {
-    dropDownContent.style.display = "none";
-  }
-});
+// // luk dropdown hvis der klikkes udenfor //
+// window.addEventListener("click", (event) => {
+//   if (!event.target.matches(".dropdown #knap1")) {
+//     dropDownContent.style.display = "none";
+//   }
+// });
 
-// hent dropdown knap //
-const button2 = document.querySelector(".dropdown2 #knap2");
-const dropDownContent2 = document.querySelector(".content2");
+// // hent dropdown knap //
+// const button2 = document.querySelector(".dropdown2 #knap2");
+// const dropDownContent2 = document.querySelector(".content2");
 
-// tilføj klik-event //
-button2.addEventListener("click", () => {
-  // tjek om dropdown allerede er vist //
-  if (dropDownContent2.style.display === "block") {
-    dropDownContent2.style.display = "none";
-  } else {
-    dropDownContent2.style.display = "block";
-  }
-});
+// // tilføj klik-event //
+// button2.addEventListener("click", () => {
+//   // tjek om dropdown allerede er vist //
+//   if (dropDownContent2.style.display === "block") {
+//     dropDownContent2.style.display = "none";
+//   } else {
+//     dropDownContent2.style.display = "block";
+//   }
+// });
 
-// luk dropdown hvis der klikkes udenfor //
-window.addEventListener("click", (event) => {
-  if (!event.target.matches(".dropdown2 #knap2")) {
-    dropDownContent2.style.display = "none";
-  }
-});
+// // luk dropdown hvis der klikkes udenfor //
+// window.addEventListener("click", (event) => {
+//   if (!event.target.matches(".dropdown2 #knap2")) {
+//     dropDownContent2.style.display = "none";
+//   }
+// });
 
-// hent dropdown knap //
-const button3 = document.querySelector(".dropdown1 #knap");
-const dropDownContent3 = document.querySelector(".content1");
+// // hent dropdown knap //
+// const button3 = document.querySelector(".dropdown1 #knap");
+// const dropDownContent3 = document.querySelector(".content1");
 
-// tilføj klik-event //
-button3.addEventListener("click", () => {
-  // tjek om dropdown allerede er vist //
-  if (dropDownContent3.style.display === "block") {
-    dropDownContent3.style.display = "none";
-  } else {
-    dropDownContent3.style.display = "block";
-  }
-});
+// // tilføj klik-event //
+// button3.addEventListener("click", () => {
+//   // tjek om dropdown allerede er vist //
+//   if (dropDownContent3.style.display === "block") {
+//     dropDownContent3.style.display = "none";
+//   } else {
+//     dropDownContent3.style.display = "block";
+//   }
+// });
 
-// luk dropdown hvis der klikkes udenfor //
-window.addEventListener("click", (event) => {
-  if (!event.target.matches(".dropdown1 #knap")) {
-    dropDownContent3.style.display = "none";
-  }
-});
+// // luk dropdown hvis der klikkes udenfor //
+// window.addEventListener("click", (event) => {
+//   if (!event.target.matches(".dropdown1 #knap")) {
+//     dropDownContent3.style.display = "none";
+//   }
+// });
 
-// hent dropdown knap //
-const button4 = document.querySelector(".dropdown3 #knap3");
-const dropDownContent4 = document.querySelector(".content3");
+// // hent dropdown knap //
+// const button4 = document.querySelector(".dropdown3 #knap3");
+// const dropDownContent4 = document.querySelector(".content3");
 
-// tilføj klik-event //
-button4.addEventListener("click", () => {
-  // tjek om dropdown allerede er vist //
-  if (dropDownContent4.style.display === "block") {
-    dropDownContent4.style.display = "none";
-  } else {
-    dropDownContent4.style.display = "block";
-  }
-});
+// // tilføj klik-event //
+// button4.addEventListener("click", () => {
+//   // tjek om dropdown allerede er vist //
+//   if (dropDownContent4.style.display === "block") {
+//     dropDownContent4.style.display = "none";
+//   } else {
+//     dropDownContent4.style.display = "block";
+//   }
+// });
 
-// luk dropdown hvis der klikkes udenfor //
-window.addEventListener("click", (event) => {
-  if (!event.target.matches(".dropdown3 #knap3")) {
-    dropDownContent4.style.display = "none";
-  }
-});
+// // luk dropdown hvis der klikkes udenfor //
+// window.addEventListener("click", (event) => {
+//   if (!event.target.matches(".dropdown3 #knap3")) {
+//     dropDownContent4.style.display = "none";
+//   }
+// });
